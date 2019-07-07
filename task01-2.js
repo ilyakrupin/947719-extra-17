0a73769bbba290acba642b8b79b6525162e0704b
Ilya Krupin <dev@krupibox.ru>
2019-07-08 00:09:25 +0300

    Add code

---
+++ task01-2.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var declOfNum = function(number, titles) {
+  cases = [2, 0, 1, 1, 1, 2];
+  return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+}
+
+declOfNum(14, ['арбуз', 'арбуза', 'арбузов']);
